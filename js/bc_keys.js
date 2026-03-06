478b70f4bff049df31a681d5959d1d37cabf1af3
nschatz27 <akschatz1s@capetigers.com>
2026-03-06 12:57:56 -0600

    fix js

--- js/bc_keys.js
+++ js/bc_keys.js
@@ -72,7 +72,40 @@ for (var i = 0; i < keyWords.length; i++) {
 }
 
 function makeKeyStyles() {
-   document.styleSheets[document.styleSheets.length - 1].insertRule("aside#keywords { " + "border: 3px solid rgb(101, 101, 101);" + "float: right;" + "margin: 20px 0px 20px 20px;" + "padding: 10px;" + "width: 320px;" + "}" , 0
+   document.styleSheets[document.styleSheets.length - 1].insertRule(
+      "aside#keywords { " + 
+      "border: 3px solid rgb(101, 101, 101);" + 
+      "float: right;" + 
+      "margin: 20px 0px 20px 20px;" + 
+      "padding: 10px;" + "width: 320px;" + 
+      "}" , 0
+   );
+
+   document.styleSheets[document.styleSheets.length -1].insertRule(
+      "aside#keywords h1 { " + 
+      "font-size: 2em;" + 
+      "margin: 5px;" + 
+      "text-align: center;" + " } ", 1
+   ); 
+
+   document.styleSheets[document.styleSheets.length -1].insertRule(
+      "aside#keywords ol { " + 
+      "font-size: 1.2em;" +
+      "margin-left: 20px;" +
+   "}" , 2
+   );
+
+   document.styleSheets[document.styleSheets.length -1].insertRule(
+      "aside#keywords ol li { " +
+      "line-height: 1.5em;" +
+      "{" , 3
+   );
+
+   document.styleSheets[document.styleSheets.length - 1].insertRule(
+      "aside#keywords ol li a { " +
+      "color: rgb(101, 101m 101);" +
+      "text-decoration: none;" +
+      "}" , 4
    );
 }
 
